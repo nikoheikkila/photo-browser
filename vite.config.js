c58a53758c7e5a6a02dae40e1f52c8449980ff59
Niko Heikkilä <niko.heikkila@futurice.com>
2022-12-25 15:27:05 +0200

    chore: initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	fs: {
+		allow: ['..']
+	}
+};
+
+export default config;
