2a81f90e60e7b6b438fe2fd6c71fa0d3f77d7fd5
Niko Heikkilä <niko.heikkila@futurice.com>
2022-12-29 18:03:04 +0200

    feat: add functions for parsing photo sizes from the URL

--- src/domain/Photo.test.ts
+++ src/domain/Photo.test.ts
@@ -1,5 +1,32 @@
 import { describe, expect, test } from 'vitest';
-import { createPhoto } from './Photo';
+import { createPhoto, parseFullSize, parseThumbnailSize } from './Photo';
+import type { Photo } from './Photo';
+import { faker } from '@faker-js/faker';
+
+const randomPhoto = (extra: Partial<Photo> = {}): Photo => {
+	return {
+		id: faker.datatype.number({ min: 1 }),
+		albumId: faker.datatype.number({ min: 1 }),
+		title: faker.random.words(),
+		url: new URL(faker.internet.url()),
+		thumbnailUrl: new URL(faker.internet.url()),
+		...extra
+	};
+};
+
+const verifyPhotoHasExactWidthAndHeight = (photo: Photo, width: number, height: number) => {
+	const size = parseFullSize(photo);
+
+	expect(size.width).toBe(width);
+	expect(size.height).toBe(height);
+};
+
+const verifyThumbnailHasExactWidthAndHeight = (photo: Photo, width: number, height: number) => {
+	const size = parseThumbnailSize(photo);
+
+	expect(size.width).toBe(width);
+	expect(size.height).toBe(height);
+};
 
 describe('Photo', () => {
 	describe('creation', () => {
@@ -25,8 +52,8 @@ describe('Photo', () => {
 				thumbnailUrl: 'https://via.placeholder.com/150/92c952'
 			});
 
-			expect(photo.url).toBeInstanceOf(URL);
-			expect(photo.thumbnailUrl).toBeInstanceOf(URL);
+			expect(photo.url.href).toBe('https://via.placeholder.com/600/92c952');
+			expect(photo.thumbnailUrl.href).toBe('https://via.placeholder.com/150/92c952');
 		});
 
 		test('throws error with malformed URLs', () => {
@@ -36,6 +63,38 @@ describe('Photo', () => {
 					thumbnailUrl: new URL('non-url')
 				})
 			).toThrowError('Invalid URL');
+		});
+	});
+
+	describe('parsing photo dimensions', () => {
+		test('returns full width and height of the photo', () => {
+			const photo = randomPhoto({
+				url: new URL('https://via.placeholder.com/640/92c952'),
+				thumbnailUrl: new URL('https://via.placeholder.com/128/92c952')
+			});
+
+			verifyPhotoHasExactWidthAndHeight(photo, 640, 640);
+			verifyThumbnailHasExactWidthAndHeight(photo, 128, 128);
+		});
+
+		test('returns default width and height for invalid photo', () => {
+			const photo = randomPhoto({
+				url: new URL('https://via.placeholder.com/92c952'),
+				thumbnailUrl: new URL('https://via.placeholder.com/92c952')
+			});
+
+			verifyPhotoHasExactWidthAndHeight(photo, 600, 600);
+			verifyThumbnailHasExactWidthAndHeight(photo, 150, 150);
+		});
+
+		test('returns default width and height for zero dimension', () => {
+			const photo = randomPhoto({
+				url: new URL('https://via.placeholder.com/0/92c952'),
+				thumbnailUrl: new URL('https://via.placeholder.com/0/92c952')
+			});
+
+			verifyPhotoHasExactWidthAndHeight(photo, 600, 600);
+			verifyThumbnailHasExactWidthAndHeight(photo, 150, 150);
 		});
 	});
 });
--- src/domain/Photo.ts
+++ src/domain/Photo.ts
@@ -34,3 +34,25 @@ export function createPhoto(args: Record<string, unknown>): Photo {
 		throw error;
 	}
 }
+
+type Dimensions = { width: number; height: number };
+
+export const parseFullSize = (photo: Photo) =>
+	parseSizeFromURL(photo.url, { width: 600, height: 600 });
+
+export const parseThumbnailSize = (photo: Photo) =>
+	parseSizeFromURL(photo.thumbnailUrl, { width: 150, height: 150 });
+
+const parseSizeFromURL = (url: URL, fallback: Dimensions): Dimensions => {
+	const dimension = url.pathname.match(/^\/(\d+)\/(.+)$/)?.at(1);
+	const result = Number.parseInt(dimension || '', 10);
+
+	if (Number.isNaN(result) || result === 0) {
+		return fallback;
+	}
+
+	return {
+		width: result,
+		height: result
+	};
+};
