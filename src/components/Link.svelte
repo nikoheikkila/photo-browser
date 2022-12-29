07b4bc663cc176b6e386368dfbfca9caae8cc871
Niko Heikkilä <niko.heikkila@futurice.com>
2022-12-29 13:26:23 +0200

    feat: add Link component

---
+++ src/components/Link.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	export let to: string;
+	export let className = '';
+</script>
+
+<a class={className} href={base + to} data-sveltekit-preload-data="tap">
+	<slot />
+</a>
