4b997f62489f9c5b1e4ab82f92fb35ab390734dc
Niko Heikkilä <niko.heikkila@futurice.com>
2022-12-30 18:28:51 +0200

    feat: add generic Warning component

---
+++ src/components/Warning.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let title: string;
+	export let message: string;
+</script>
+
+<section
+	role="alert"
+	class="drac-card-subtle drac-border-yellow drac-w-3xl centered drac-text drac-text-white drac-p-md"
+>
+	<h2 class="drac-text-bold">{title}</h2>
+	<p>{message}</p>
+</section>
