aa302bde6936be1a550c889656c598358ed0f05d
Niko Heikkilä <niko.heikkila@futurice.com>
2022-12-30 18:29:44 +0200

    refactor: make Error component less interactive

--- src/components/Error.svelte
+++ src/components/Error.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
 	export let message: string;
-
-	let detailsOpen = false;
-
-	const toggleDetails = () => {
-		detailsOpen = !detailsOpen;
-	};
 </script>
 
 <section
@@ -13,26 +7,5 @@
 	class="drac-card-subtle drac-border-red drac-w-3xl centered drac-text drac-text-white drac-p-md"
 >
 	<h2 class="drac-text-bold">There was an error loading the page</h2>
-
-	<input
-		type="checkbox"
-		name="details"
-		id="details"
-		class="drac-checkbox drac-checkbox-purple"
-		checked={detailsOpen}
-		on:change={toggleDetails}
-	/>
-	<label for="details" class="drac-text drac-text-white">
-		{#if detailsOpen}
-			Hide details
-		{:else}
-			Show details
-		{/if}
-	</label>
-
-	{#if detailsOpen}
-		<p>
-			<code>{message}</code>
-		</p>
-	{/if}
+	<p>{message}</p>
 </section>
