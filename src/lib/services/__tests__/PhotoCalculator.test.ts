1e22e32c04e627a5f08e2f4f83649e93787e72d9
Niko Heikkilä <niko.heikkila@futurice.com>
2023-01-07 14:38:23 +0200

    refactor: improve photo calculator unit tests
    [skip netlify]

--- src/lib/services/__tests__/PhotoCalculator.test.ts
+++ src/lib/services/__tests__/PhotoCalculator.test.ts
@@ -1,59 +1,61 @@
 import { PhotoCalculator } from '../PhotoCalculator';
 import { randomPhoto } from './';
+import { faker } from '@faker-js/faker';
+import type { Photo } from '$lib/domain/Photo';
+import type { Dimensions } from '$lib/domain/Schema';
 
 describe('Photo Calculator', () => {
 	test('returns the width and height of a photo', () => {
-		const photo = randomPhoto({
-			url: new URL('https://via.placeholder.com/640/92c952'),
-			thumbnailUrl: new URL('https://via.placeholder.com/128/92c952')
-		});
-		const calculator = new PhotoCalculator(photo);
+		const full = faker.datatype.number({ min: 1 });
+		const thumbnail = faker.datatype.number({ min: 1 });
+		const nonce = faker.datatype.string();
 
-		verifyPhotoHasExactWidthAndHeight(calculator, 640, 640);
-		verifyThumbnailHasExactWidthAndHeight(calculator, 128, 128);
+		const photo = randomPhoto({
+			url: randomURL(full, nonce),
+			thumbnailUrl: randomURL(thumbnail, nonce)
+		});
+
+		expectPhotoHasExactSize(photo, { width: full, height: full });
+		expectThumbnailHasExactSize(photo, { width: thumbnail, height: thumbnail });
 	});
 
 	test('returns default width and height for photo with invalid URL', () => {
-		const photo = randomPhoto({
-			url: new URL('https://via.placeholder.com/92c952'),
-			thumbnailUrl: new URL('https://via.placeholder.com/92c952')
-		});
-		const calculator = new PhotoCalculator(photo);
+		const nonce = faker.datatype.string();
 
-		verifyPhotoHasExactWidthAndHeight(calculator, 600, 600);
-		verifyThumbnailHasExactWidthAndHeight(calculator, 150, 150);
+		const photo = randomPhoto({
+			url: new URL(`https://via.placeholder.com/${nonce}`),
+			thumbnailUrl: new URL(`https://via.placeholder.com/${nonce}`)
+		});
+
+		expectPhotoHasExactSize(photo, { width: 600, height: 600 });
+		expectThumbnailHasExactSize(photo, { width: 150, height: 150 });
 	});
 
 	test('returns default width and height for URl with zero dimension', () => {
-		const photo = randomPhoto({
-			url: new URL('https://via.placeholder.com/0/92c952'),
-			thumbnailUrl: new URL('https://via.placeholder.com/0/92c952')
-		});
-		const calculator = new PhotoCalculator(photo);
+		const size = 0;
+		const nonce = faker.datatype.string();
 
-		verifyPhotoHasExactWidthAndHeight(calculator, 600, 600);
-		verifyThumbnailHasExactWidthAndHeight(calculator, 150, 150);
+		const photo = randomPhoto({
+			url: randomURL(size, nonce),
+			thumbnailUrl: randomURL(size, nonce)
+		});
+
+		expectPhotoHasExactSize(photo, { width: 600, height: 600 });
+		expectThumbnailHasExactSize(photo, { width: 150, height: 150 });
 	});
 
-	const verifyPhotoHasExactWidthAndHeight = (
-		calculator: PhotoCalculator,
-		width: number,
-		height: number
-	) => {
-		const size = calculator.parseFullSize();
+	const expectPhotoHasExactSize = (photo: Photo, expected: Dimensions) => {
+		const actual = new PhotoCalculator(photo).parseFullSize();
 
-		expect(size.width).toBe(width);
-		expect(size.height).toBe(height);
+		expect(actual).toStrictEqual(expected);
 	};
 
-	const verifyThumbnailHasExactWidthAndHeight = (
-		calculator: PhotoCalculator,
-		width: number,
-		height: number
-	) => {
-		const size = calculator.parseThumbnailSize();
+	const expectThumbnailHasExactSize = (photo: Photo, expected: Dimensions) => {
+		const actual = new PhotoCalculator(photo).parseThumbnailSize();
 
-		expect(size.width).toBe(width);
-		expect(size.height).toBe(height);
+		expect(actual).toStrictEqual(expected);
 	};
+
+	const randomURL = (size: number, nonce = '') =>
+		new URL(`${faker.internet.url()}/${size}/${nonce}`);
 });
