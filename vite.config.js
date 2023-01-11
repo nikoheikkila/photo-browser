001cae5dce311e7d3f7117120716d14cb8812369
Niko Heikkilä <niko.heikkila@futurice.com>
2023-01-11 19:45:20 +0200

    refactor: move unit test config to single file

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
-const isPipeline = !!process.env.CI;
-
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.test.ts'],
-		reporters: ['verbose'],
-		allowOnly: !isPipeline,
-		globals: true,
-		environment: 'node',
-		cache: {
-			dir: '/tmp/.vitest-cache'
-		},
-		typecheck: {
-			checker: 'tsc'
-		},
-		sequence: {
-			shuffle: true
-		},
-		coverage: {
-			enabled: true,
-			provider: 'c8',
-			reporter: ['text', 'html']
-		}
-	},
 	fs: {
 		allow: ['..']
 	}
---
+++ vitest.unit.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+const isPipeline = !!process.env.CI;
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.test.ts'],
+		reporters: ['verbose'],
+		allowOnly: !isPipeline,
+		globals: true,
+		environment: 'node',
+		cache: {
+			dir: '/tmp/.vitest-cache'
+		},
+		typecheck: {
+			checker: 'tsc'
+		},
+		sequence: {
+			shuffle: true
+		},
+		coverage: {
+			enabled: true,
+			provider: 'c8',
+			reporter: ['text', 'html']
+		}
+	}
+});
